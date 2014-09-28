fd2c65885ab349ec365c17670f5681ffe6142e4b
masapiyo <pingu342@users.noreply.github.com>
2014-09-28 23:37:45 +0900

    スピーカON/OFFボタン追加

--- src/jp/saka/ringbacktone/RingbacktoneActivity.java
+++ src/jp/saka/ringbacktone/RingbacktoneActivity.java
@@ -23,6 +23,9 @@ public class RingbacktoneActivity extends Activity
 		mRingbacktone = Ringbacktone.getInstance(this);
 		mAudioManager = ((AudioManager) getSystemService(Context.AUDIO_SERVICE));
 
+		mAudioManager.setSpeakerphoneOn(true);
+		mAudioManager.setMode(AudioManager.MODE_IN_COMMUNICATION);
+
 		Button button;
 		button = (Button)findViewById(R.id.PlayButton);
 		button.setOnClickListener(new OnClickListener() {
@@ -34,6 +37,18 @@ public class RingbacktoneActivity extends Activity
 		button.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				mRingbacktone.stop();
+			}
+		});
+		button = (Button)findViewById(R.id.SpeakerphoneOn);
+		button.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				mAudioManager.setSpeakerphoneOn(true);
+			}
+		});
+		button = (Button)findViewById(R.id.SpeakerphoneOff);
+		button.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				mAudioManager.setSpeakerphoneOn(false);
 			}
 		});
 
